0013d22d2a0c622147a580742636993ccd480042
SadickYahaya <abubakar.yahaya@amalitech.org>
2022-08-26 21:07:09 +0000

    Jobs list page

--- src/Components/JobsSection/Jobs.tsx
+++ src/Components/JobsSection/Jobs.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import Job from "./Job/Job";
 import { JobsContainer, JobsSection } from "./Jobs.styles";
-import { jobInfo, jobInfo1, jobInfo2, jobInfo3, jobInfo4 } from "../../Utils/JobData";
+import {
+  jobInfo,
+  jobInfo1,
+  jobInfo2,
+  jobInfo3,
+  jobInfo4,
+} from "../../Utils/JobData";
+import JobTypeCheckbox from "../../Utils/Checkbox";
+import JobLocationRadioButtons from "../../Utils/Radio";
 
 const Jobs = () => {
-  
   return (
     <JobsContainer>
       <h3>Jobs Found</h3>
       <JobsSection>
-        <div className="sidebar"></div>
+        <div className="sidebar">
+          <span>
+            <JobTypeCheckbox />
+          </span>
+          <span className="radio-buttons">
+            <JobLocationRadioButtons />
+          </span>
+        </div>
         <div className="jobs">
           <Job {...jobInfo} />
           <Job {...jobInfo1} />
